904fa7c58df2ecb62b048d6c3e33b65d998caa16
Tim Fitzgerald <diceytroop@gmail.com>
2020-12-02 15:19:26 -0500

    Committed deletions

--- stemmy-node/src/Controllers/LoopyImporter/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './LoopyAiffFileReader';
-export * from './LoopyPlistReader';
-export * from './LoopySingleSessionReader';
-export * from './LoopyMultiSessionReader';
--- stemmy-node/src/Models/AudioEntities/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AudioEntity';
-export * from './Loop';
-
-export enum audioEntityTypes {
-  Loop,
-}
